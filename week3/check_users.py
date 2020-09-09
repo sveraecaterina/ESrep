fc47af4621dd5dd5c4433b62c4caa3c2dfde403c
sveraecaterina <70235323+sveraecaterina@users.noreply.github.com>
2020-09-09 17:36:32 -0400

    Update check_users.py

--- week3/check_users.py
+++ week3/check_users.py
@@ -19,5 +19,5 @@ def check_users(current_users, new_users):
 
 if __name__ == "__main__":
     current_us = ['chris','haritha', 'sally', 'darnell', 'superman']
-    new_us = ['George', 'ringo', 'Superman', 'hannibal']
+    new_us = ['george', 'ringo', 'superman', 'hannibal']
     check_users(current_us, new_us)
