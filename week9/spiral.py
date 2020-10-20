87112df89c8503ce52a48f0279644c2915c63182
sveraecaterina <70235323+sveraecaterina@users.noreply.github.com>
2020-10-20 15:03:46 -0400

    Update spiral.py

--- week9/spiral.py
+++ week9/spiral.py
@@ -1,19 +1,19 @@
 def spiral(x):
 #im sorry if some of this is redundant, i spent hours on this i'm afraid to break it now
 
-    a=1
+    sum=1
     val=0
 
     my_list=[y for y in range(1,x) if y%2==0 ] 
     for item in my_list:
  
         for i in range(1,5):
-            a+=1+i*(item)+val
+            sum+=1+i*(item)+val
             i+=1
 
         val+=4*(item)
 
-    return print(a)
+    return print(sum)
 
 if __name__ == "__main__":
     x=501
