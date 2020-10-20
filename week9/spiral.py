8cac04ef304297395c863d3dd2b253f022508367
sveraecaterina <70235323+sveraecaterina@users.noreply.github.com>
2020-10-20 14:52:27 -0400

    Update spiral.py

--- week9/spiral.py
+++ week9/spiral.py
@@ -1,19 +1,14 @@
 def spiral(x):
 #im sorry if some of this is redundant, i spent hours on this i'm afraid to break it now
 
-    a=0
-    i=1
-    value=1
+    a=1
     val=0
 
     my_list=[y for y in range(1,x) if y%2==0 ] 
     for item in my_list:
  
         for i in range(1,5):
-            
-            a=value
             a+=1+i*(item)+val
-            value=a
             i+=1
 
         val+=4*(item)
